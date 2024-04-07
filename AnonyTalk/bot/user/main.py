56c340ae7151411fc0fe94f98a8c726e6dc33ab9
AnonyStarVoyager <116251604+AnonyStarVoyager@users.noreply.github.com>
2024-04-07 22:37:22 +0330

    Update main.py

--- AnonyTalk/bot/user/main.py
+++ AnonyTalk/bot/user/main.py
@@ -1,5 +1,6 @@
 from bot.user import command_start
 from bot.user import command_update
+from bot.user import command_github
 from bot.user import cancel
 from bot.user import help
 from bot.user import technicalـdocumentation
