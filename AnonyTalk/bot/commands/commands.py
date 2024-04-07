faf605a55726e75769a34c43385a7fc5ad03ba81
AnonyStarVoyager <116251604+AnonyStarVoyager@users.noreply.github.com>
2024-04-07 20:46:47 +0330

    Update commands.py

--- AnonyTalk/bot/commands/commands.py
+++ AnonyTalk/bot/commands/commands.py
@@ -7,8 +7,8 @@ async def set_bot_commands():
         commands=[
             telebot.types.BotCommand("start", "♻️ بزن بریم! همین الان شروع کنیم؟"),
             telebot.types.BotCommand("update", "🔄 آخرین خبرا و آپدیت‌ها رو اینجا چک کن!"),
-            telebot.types.BotCommand("github", "🐙🐱 کد‌های منو اینجا ببین!"),
-            telebot.types.BotCommand("donation", "💖 دوست داری حمایتم کنی؟")
+            telebot.types.BotCommand("github", "🐙🐱 کد‌های منو اینجا ببین!")
+            # telebot.types.BotCommand("donation", "💖 دوست داری حمایتم کنی؟")
         ],
         scope=telebot.types.BotCommandScopeDefault()
     )
