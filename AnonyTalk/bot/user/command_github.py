04d9c129b81989170b547e9fc8b72532ffd65560
AnonyStarVoyager <116251604+AnonyStarVoyager@users.noreply.github.com>
2024-04-07 22:28:16 +0330

    Create command_github.py

---
+++ AnonyTalk/bot/user/command_github.py
@@ -0,0 +1,15 @@
+from bot.base import *
+
+@bot.message_handler(commands=['github'],content_types=['text'],is_ban=False,user_status=False)
+async def update_message(message: telebot.types.Message):
+    chat_id = message.chat.id
+    await bot.send_sticker(chat_id,"CAACAgIAAxkBAAEL1wABZg18Agglcha726Ic_HORJQ6at4MAAk4CAAJWnb0KMP5rbYEyA280BA")
+    text = """
+    <b>
+    🛡️ حفظ حریم خصوصی یک اصل مهم و غیرقابل چشم‌پوشی است
+    🛠️ قبل از شروع به کار با ربات، لطفاً با دقت مستندات زیر را مطالعه کنید تا با خیالی آسوده از فضای مجازی لذت ببرید
+    </b>
+    """
+    github = telebot.types.InlineKeyboardMarkup()
+    github.add(telebot.types.InlineKeyboardButton(text="🐙🐱 کد‌های منو اینجا ببین!",url="https://github.com/AnonyStarVoyager/AnonyTalk"))
+    await bot.send_document(chat_id,"BQACAgQAAxkBAAM2Zg2vAAEyq8daM2-V2U9ditaKn2rqAAKLEgACE9RpULf742yAh2lUNAQ",caption=text,reply_markup=github)
